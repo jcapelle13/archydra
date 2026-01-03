e008b1f1f8f273af80431cb6437869241ab0583c
Jared Capelle <14522128+jcapelle13@users.noreply.github.com>
2026-01-03 13:07:56 -0500

    Infer selector page size from terminal window

--- src/archydra/Filters/Selector.py
+++ src/archydra/Filters/Selector.py
@@ -3,14 +3,23 @@ from archydra.AbstractClasses import Filter, Producer
 from itertools import islice
 from loguru import logger
 import click
+import os
+
 
 class Selector(Filter):
     """
     Filter that only outputs urls that user selects
     """
-    def __init__(self, producers: list[Producer], page_size=2) -> None:
+
+    def __init__(self, producers: list[Producer], page_size=0) -> None:
         super().__init__(producers)
-        self.page_size = page_size
+        if page_size > 0:
+            self.page_size = page_size
+        else:
+            window_size = os.get_terminal_size()
+            logger.trace("Terminal size: {}", window_size)
+            self.page_size = window_size.lines - 3
+        logger.debug("Set page size to {}", self.page_size)
 
     def get_urls(self) -> Iterable[str]:
         rr_gen = self._roundrobin()
